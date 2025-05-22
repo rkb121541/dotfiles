2278e0292c84ad73b407022e1cb3d1ce8ac6e85e
rkb121541 <rkbidarkota@gmail.com>
2025-05-22 17:24:34 -0400

    backing up

---
+++ nvim/lua/plugins/lsp-config.lua
@@ -0,0 +1,39 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+      vim.keymap.set("n", "<leader>m", ":Mason<CR>", {})
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = {
+          "lua_ls",
+          "clangd",
+          -- "cmake"
+        },
+      })
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      local capabilities = require('cmp_nvim_lsp').default_capabilities()
+      local lspconfig = require("lspconfig")
+      lspconfig.lua_ls.setup({
+        capabilities = capabilities
+      })
+      lspconfig.clangd.setup({
+        capabilities = capabilities
+      })
+      -- lspconfig.cmake.setup({
+      --   capabilities = capabilities
+      -- })
+      vim.keymap.set("n", "<leader>h", vim.lsp.buf.hover, {})
+      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+    end,
+  },
+}
